9442b4ffe7fd96b8c564e7c56d7cf9d8cf63a6ce
Omeed Golkaryeh <omeedgolkaryeh@macbook-pro.myfiosgateway.com>
2021-04-18 15:32:00 -0400

    testagain

--- testfile.py
+++ testfile.py
@@ -1,7 +1,13 @@
+
 import numpy as np
+import pandas as pd
+
+FILENAME = "world_dev_ind.csv"
 
 
 if __name__ == '__main__':
-  numbers = np.arange(21)
-  print(len(numbers))
   
+  df = pd.read_csv(FILENAME)
+  pd.set_option("display.max.columns", None)
+  pd.set_option("display.precision", 2)
+  print(df.describe())
